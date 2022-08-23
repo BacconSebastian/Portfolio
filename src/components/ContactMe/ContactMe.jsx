ac4f6603b99310d4893d8d893be81d28fa2b568e
Sebastian Baccon <bacconsebastian@gmail.com>
2022-08-23 17:23:41 -0300

    Ortografy fix

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -20,7 +20,7 @@ const ContactMe = () => {
             <h2>ContactMe</h2>
             <h3>Give me a chance to show you what I can do</h3>
             <hr></hr>
-            <p>I am looking for an opportunity to start working in the IT fiel and I am willing to solve any type of doubt, contact me by:</p>
+            <p>I am looking for an opportunity to start working in the IT field and I am willing to solve any type of doubt, contact me by:</p>
             <a href="mailto:bacconsebastian@gmail.com">bacconsebastian@gmail.com</a>
         </div>
     )
